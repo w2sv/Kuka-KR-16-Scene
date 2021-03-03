f4d78aa98dd34f754a1907f3a8ddcd264a9a5918
Janek Zangenberg <zangenbergjanek@googlemail.com>
2021-03-03 16:38:08 +0100

    We Init

--- src/camera.cpp
+++ src/camera.cpp
@@ -12,7 +12,7 @@ Camera::Camera(Robot* robot) :
 
 
 void Camera::toggleMode() {
-	static const int TOGGLE_MODES = 2;
+	static const int TOGGLE_MODES = 3;
 
 	this->mode = Mode((this->mode + 1) % TOGGLE_MODES);
 }
@@ -28,6 +28,8 @@ const void Camera::set() {
 	switch (mode) {
 		case Observer:
 			setObserverMode(); break;
+		case Walk:
+			setWalkMode(); break;
 		case TCP:
 			setTCPMode(); break;
 		case Gyrate:
@@ -49,6 +51,8 @@ void Camera::setCameraParameterAccordingly() {
 	double phi = 0.2 * position.x / cg_globState::screenSize[0] * M_PI + M_PI * 0.5;
 	double theta = 0.2 * position.y / cg_globState::screenSize[1] * M_PI;
 
+	std::cout << theta << std::endl;
+
 	double eyePosX = radius * cos(phi) * cos(theta);
 	double eyePosY = radius * sin(theta);
 	double eyePosZ = radius * sin(phi) * cos(theta);
@@ -60,12 +64,23 @@ void Camera::setCameraParameterAccordingly() {
 
 
 void Camera::updateRadiusViaScroll() {
-	static const float WHEEL_EVENT_RADIUS_INCREMENT = 1.5;
+	static const float DELTA_RADIUS = 1.5;
 
-	if (cg_mouse::buttonState(GLUT_MOUSE_WHEEL_UP))
-		radius -= WHEEL_EVENT_RADIUS_INCREMENT;
+	if (cg_mouse::buttonState(GLUT_MOUSE_WHEEL_UP)) {
+		radius -= DELTA_RADIUS;
+		floorRadius();
+	}
 	else if (cg_mouse::buttonState(GLUT_MOUSE_WHEEL_DOWN))
-		radius += WHEEL_EVENT_RADIUS_INCREMENT;
+		radius += DELTA_RADIUS;
+}
+void Camera::updateRadiusViaWheelPressing() {
+	if (cg_mouse::buttonState(GLUT_MIDDLE_BUTTON)) {
+		radius = std::max<float>(radius + 0.1 * cg_mouse::moveY(), 1.0);
+		floorRadius();
+	}
+}
+void Camera::floorRadius() {
+	radius = std::max<float>(radius, RADIUS_MIN);
 }
 #pragma endregion
 
@@ -79,24 +94,37 @@ void Camera::setObserverMode() {
 		position.x += cg_mouse::moveX();
 		position.y += cg_mouse::moveY();
 	}
-
-	// Zooming via scroll event
+	
 	updateRadiusViaScroll();
+	updateRadiusViaWheelPressing();
+	
+	setCameraParameterAccordingly();
+}
+void Camera::setWalkMode() {
+	const static float DELTA_POSITION = 0.1;
+	
+	static float xStep = 0;
+	static float yStep = 0;
 
-	// Zooming via mouse movement during pressing of mouse wheel
-	if (cg_mouse::buttonState(GLUT_MIDDLE_BUTTON))
-		radius = std::max<float>(radius + 0.1 * cg_mouse::moveY(), 1.0);
+	if (cg_key::specialKeyState(GLUT_KEY_DOWN))
+		yStep -= DELTA_POSITION;
+	if (cg_key::specialKeyState(GLUT_KEY_UP))
+		yStep += DELTA_POSITION;
+	if (cg_key::specialKeyState(GLUT_KEY_LEFT))
+		xStep += DELTA_POSITION;
+	if (cg_key::specialKeyState(GLUT_KEY_RIGHT))
+		xStep -= DELTA_POSITION;
+
+	Axes::X::translate(xStep);
+	Axes::Y::translate(yStep);
 
 	setCameraParameterAccordingly();
 }
-
-
 void Camera::setTCPMode() {
 	// TODO: debug
 
-	this->robot->assumeSpatialTCPConfiguration();
+	robot->assumeSpatialTCPConfiguration();
 }
-
 
 void Camera::setGyrateMode() {
 	static const float X_POSITION_INCREMENT_PER_FRAME = 0.005;
--- src/camera.h
+++ src/camera.h
@@ -25,24 +25,29 @@ public:
 private:
 	enum Mode {
 		Observer,
+		Walk,
 		TCP,
 		Gyrate
 	};
 
 	const Vector2 START_POSITION = Vector2(0, cg_globState::screenSize[1] * 0.5);
 	const float START_RADIUS = 40;
-
-	Robot* robot;
+	const float RADIUS_MIN = 7.4;
 
 	Vector2 position;
 	Mode mode;
-
 	float radius;
 
+	Robot* robot;
+
 	void setObserverMode();
+	void setWalkMode();
 	void setTCPMode();
 	void setGyrateMode();
 
-	void updateRadiusViaScroll();
 	void setCameraParameterAccordingly();
+
+	void updateRadiusViaScroll();
+	void updateRadiusViaWheelPressing();
+	void floorRadius();
 };
